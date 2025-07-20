a7bb53bf199639e59eabc337be6089a6130ba2d6
venkatraman-v <vvaratharaja@umass.edu>
2025-07-20 10:09:25 -0400

    finished chall 10

--- chall_10_cbc.py
+++ chall_10_cbc.py
@@ -1,21 +1,8 @@
-from set_01_utils import base64_to_bytes
+from set_01_utils import base64_to_bytes, ecb_encrypt, ecb_decrypt, xor, base64file_to_bytes, cbc_decrypt
 # CBC is a mode
 # each ciphtertext block is added to the next palintext block??
 
-def base64file_to_bytes(filename: str) -> bytes:
-    string_base64 = ""
-    with open(filename) as file:
-        for line in file:
-            string_base64 += line.strip()
-    return base64_to_bytes(string_base64)
 
 if __name__ == "__main__":
-    ciphertext_base64 = ""
-    with open("chall_08_input.txt") as file:
-        for line in file:
-            ciphertext_base64 += line.strip()
-
-def make_blocks(message: bytes, block_size = 8) -> "list[bytes]":
-    return [message[i: i + block_size] for i in range(0, len(message), block_size)]
-# def cbc_encrypt(message:bytes, key:bytes):
-    
+    ciphertext = base64file_to_bytes("chall_10_input.txt")
+    print(str(cbc_decrypt(ciphertext, b"YELLOW SUBMARINE", b"\x00" * 16)).replace("\\n", "\n"))    
--- set_01_utils.py
+++ set_01_utils.py
@@ -135,6 +135,7 @@ def xor(message: bytes, key: bytes) -> bytes:
     for i in range(len(message)):
         ret.append(message[i] ^ key[i % len(key)])
     return bytes(ret)
+
 def unicode_to_bytes(text: str) -> bytes:
     return bytes([ord(a_byte) for a_byte in text])
 
@@ -222,6 +223,41 @@ def pkcs_pad(message: bytes, block_size: int) -> bytes:
     return message + bytes([num_to_append for _ in range(num_to_append)])
 
 # -------------------CHALL 10--------------------------
+def base64file_to_bytes(filename: str) -> bytes:
+    string_base64 = ""
+    with open(filename) as file:
+        for line in file:
+            string_base64 += line.strip()
+    return base64_to_bytes(string_base64)
+
+def make_blocks(message: bytes, block_size) -> "list[bytes]":
+    return [message[i: i + block_size] for i in range(0, len(message), block_size)]
+
+def cbc_encrypt(message:bytes, key:bytes, iv: bytes) -> bytes:
+    assert len(key) == len(iv)
+    assert len(key) == 16
+    plaintext_blocks = make_blocks(message, 16)
+    ciphertext = bytearray()
+    prev_ciphertext_block = iv
+    for plain_block in plaintext_blocks:
+        ecb_input = xor(prev_ciphertext_block, plain_block)
+        prev_ciphertext_block = ecb_encrypt(ecb_input, key)
+        ciphertext += prev_ciphertext_block
+    return bytes(ciphertext)
+
+def cbc_decrypt(ciphertext:bytes, key:bytes, iv: bytes) -> bytes:
+    assert len(key) == len(iv)
+    assert len(key) == 16
+    ciphertext_blocks = make_blocks(ciphertext, 16)
+    plaintext = bytearray()
+    prev_cipher_block = iv
+    for cipher_block in ciphertext_blocks:
+        ecb_input = ecb_decrypt(cipher_block, key)
+        plaintext += xor(ecb_input, prev_cipher_block)
+        prev_cipher_block = cipher_block
+        
+    return bytes(plaintext)
+
 # -------------------CHALL 03--------------------------
 # -------------------CHALL 03--------------------------
 # -------------------CHALL 03--------------------------
